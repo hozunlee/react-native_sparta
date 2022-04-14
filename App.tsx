015562106e8643779e461737629c87a0f7872c0a
ho2yahh <ho2yahh@gmail.com>
2022-04-14 16:54:32 +0900

    detail page 초안

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import AboutPage from "./pages/AboutPage";
+import DetailPage from "./pages/DetailPage";
 import MainPage from "./pages/MainPage";
 
 export default function App() {
     // return <MainPage />;
-    return <AboutPage />;
+    // return <AboutPage />;
+    return <DetailPage />;
 }
---
+++ pages/DetailPage.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import main from "./assets/main.png";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    ScrollView,
+} from "react-native";
+import data from "../data.json";
+
+export default function DetailPage() {
+    console.disableYellowBox = true;
+    let tip = data.tip;
+
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.mainImage}
+                resizeMode={"cover"}
+                source={{
+                    uri: tip[9].image,
+                }}
+            />
+            <View>
+                <Text style={styles.mainText}>{tip[9].title}</Text>
+                <Text style={styles.subText}>{tip[9].desc}</Text>
+                <TouchableOpacity style={styles.btn}>
+                    <Text style={styles.btnText}>팁 찜하기</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        //앱의 배경 색
+        backgroundColor: "#fff",
+        height: "100%",
+    },
+    mainImage: {
+        width: "90%",
+        height: "40%",
+        alignSelf: "center",
+        marginTop: 30,
+        borderRadius: 10,
+    },
+    mainText: {
+        fontSize: 25,
+        fontWeight: "bold",
+        textAlign: "center",
+        margin: 20,
+    },
+    subText: {
+        fontSize: 15,
+        textAlign: "center",
+        margin: 5,
+    },
+    btn: {
+        width: 100,
+        height: 50,
+        padding: 15,
+        backgroundColor: "#fdc453",
+        borderColor: "deeppink",
+        borderRadius: 15,
+        margin: 7,
+        alignSelf: "center",
+    },
+    btnText: {
+        textAlign: "center",
+        color: "white",
+    },
+});
