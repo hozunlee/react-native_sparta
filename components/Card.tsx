9cc2260dd9bb9f98ebba9d8a028dd60b52b99ef7
ho2yahh <ho2yahh@gmail.com>
2022-04-25 19:35:00 +0900

    firebase realtime db 저장하기 (찜기능)

--- components/Card.tsx
+++ components/Card.tsx
@@ -21,7 +21,7 @@ export default function Card({ content, navigation }: IProps) {
         <TouchableOpacity
             style={styles.card}
             onPress={() => {
-                navigation.navigate("DetailPage", content);
+                navigation.navigate("DetailPage", { idx: content.idx });
             }}
         >
             <Image style={styles.cardImage} source={{ uri: content.image }} />
